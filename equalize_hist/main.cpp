6e0a957d7c6f15243bb538405b29055070d4e223
Sakana <45968398+Sakana-2@users.noreply.github.com>
2023-05-06 17:43:41 +0900

    deleting record

--- equalize_hist/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <opencv2/opencv.hpp>
-
-#include "../grayscale/grayscale.hpp"
-#include "equalize_hist.hpp"
-
-int main() {
-	cv::imwrite("./equalize_hist.png",equalize_hist(grayscale(cv::imread("../assets/lenna.jpg"))));
-	return 0;
-}
--- grayscale/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "grayscale.hpp"
-#include <iostream>
-int main() {
-	//std::cout << std::round(127.5) << std::endl;
-	cv::imwrite("./MAJIDESEIKOUSIRO.png", grayscale(cv::imread("../assets/lenna.jpg")));
-	return 0;
-}
