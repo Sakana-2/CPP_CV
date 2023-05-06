7651fb6c0c691ed08de5a61102a4e67b0c7cec96
Sakana <45968398+Sakana-2@users.noreply.github.com>
2023-05-06 17:45:29 +0900

    make ganma_correction

---
+++ inio/ganma_correction.cpp
@@ -0,0 +1,13 @@
+#include "ganma_correction.hpp"
+
+cv::Mat gc(cv::Mat src, float ganma) {
+	for (int i = 0; i < src.rows; ++i)
+	{
+		uchar* row = src.ptr<uchar>(i);
+		for (int j = 0; j < src.cols; ++j)
+		{
+			row[j] = static_cast<uchar>(std::round(255 * std::pow(row[j] / 255.0, 1.0 / ganma)));
+		}
+	}
+	return src;
+}
