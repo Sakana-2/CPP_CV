2eb671b54ff5d2825ed75f7eff9aae407a48af26
Sakana <45968398+Sakana-2@users.noreply.github.com>
2023-06-20 18:57:11 +0900

    made a function to do nearbyint for cv::Mat

--- inio/util.cpp
+++ inio/util.cpp
@@ -5,3 +5,17 @@ double gaussian(double s,double rr) {
 	const double pi = 3.141592653589793;
 	return std::exp(-rr / (2 * s * s)) / (2 * pi * s * s);
 }
+
+cv::Mat mat_nearbyint(cv::Mat src) {
+	cv::Mat dst(src.rows, src.cols, CV_64FC1, cv::Scalar::all(0));
+	for (int i = 0; i < src.rows; ++i)
+	{
+		double* row = src.ptr<double>(i);
+		double* o_row = dst.ptr<double>(i);
+		for (int j = 0; j < src.cols; ++j)
+		{
+			o_row[j] = std::nearbyint(row[j]);
+		}
+	}
+	return dst;
+}
--- inio/util.hpp
+++ inio/util.hpp
@@ -1,3 +1,7 @@
 #pragma once
 
-double gaussian(double);
+#include <opencv2/opencv.hpp>
+
+double gaussian(double s, double rr);
+
+cv::Mat mat_nearbyint(cv::Mat src);
