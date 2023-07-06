6803885e9a9d25df41c60599af00303076207601
Sakana <45968398+Sakana-2@users.noreply.github.com>
2023-07-06 18:47:36 +0900

    Made gamma_correction moved by lut.

--- inio/gamma_correction.cpp
+++ inio/gamma_correction.cpp
@@ -1,16 +1,13 @@
 #include <cmath>
+
 #include "gamma_correction.hpp"
+#include "lut.hpp"
 
 cv::Mat gc(cv::Mat src, double ganma) {
-	cv::Mat dst(src.rows, src.cols, CV_64FC1, cv::Scalar::all(0));
-	for (int i = 0; i < src.rows; ++i)
+	uchar lut[256] = { 0 };
+	for (int i = 0; i < 256; ++i)
 	{
-		double* row = src.ptr<double>(i);
-		double* o_row = dst.ptr<double>(i);
-		for (int j = 0; j < src.cols; ++j)
-		{
-			o_row[j] = std::nearbyint(255 * std::pow(row[j] / 255.0, 1.0 / ganma));
-		}
+		lut[i] = std::nearbyint(255 * std::pow(i / 255.0, 1.0 / ganma));
 	}
-	return dst;
+	return LUT(src, lut);
 }
