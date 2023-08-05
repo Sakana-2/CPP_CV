40fe02bc02ed0346351b8395a664bc85d4144bcd
Sakana <45968398+Sakana-2@users.noreply.github.com>
2023-08-05 17:23:37 +0900

    Made a function of finding local maximum.

--- inio/hough.cpp
+++ inio/hough.cpp
@@ -12,34 +12,52 @@ void CreateTrigTable(std::vector<double>& tabSin, std::vector<double>& tabCos, d
 	}
 }
 
+void findLocalMaximum(int numrho, int numangle, int threshold, const int* accum, std::vector<int>& lm_indices) {
+	for (int i = 0; i < numangle; i++)
+	{
+		for (int j = 0; j < numrho; j++)
+		{
+			int base = (i + 1) * (numrho + 2) + j + 1;
+			if ((accum[base] > accum[base - 1]) && (accum[base] >= accum[base + 1]) && (accum[base] > accum[base - (numrho + 2)]) && (accum[base] >= accum[base + (numrho + 2)])) {
+				lm_indices.push_back(accum[base]);
+			}
+		}
+	}
+
+}
+
 void hl(cv::Mat src, double rho, double theta, int threshold) {
-	const double diagonal = std::sqrt(src.rows * src.rows + src.cols * src.cols);
 	int numangle = std::nearbyint(PI / theta); //計算する角度の種類の数
-	int numrho = std::nearbyint((2 * diagonal) / rho); //計算する距離の種類の数
+	int numrho = std::nearbyint((2 * (src.rows + src.cols)) / rho); //計算する距離の種類の数
 
 	std::vector<double> tabSin,tabCos;
 	CreateTrigTable(tabSin, tabCos, theta, numangle, rho); //使用するサインとコサインの値を事前に計算する
 
-	cv::Mat accum = cv::Mat::zeros(numangle + 2, numrho + 2, CV_32SC1); //端の分が足りないから+1,四捨五入のときの誤差用にもう+1
-
+	cv::Mat _accum = cv::Mat::zeros(numangle + 2, numrho + 2, CV_32SC1); //+2するのはフィルターをかけるときのためのパディング処理
+	int* accum = _accum.ptr<int>();
 	//投票
 	for (int i = 0; i < src.rows; i++)
 	{
-		double* row = src.ptr<double>(i);
 		for (int j = 0; j < src.cols; j++)
 		{
+			double* row = src.ptr<double>(i);
 			if (row[j] != 0) {
 				
 				for (int n = 0; n < numangle; n++)
 				{
 					int r = std::nearbyint(j * tabSin[n] + i * tabCos[n]);
-					r += numrho / 2;
+					r += std::nearbyint(numrho / 2);
 
-					int* accum_row = accum.ptr<int>(n);
-					accum_row[r]++;
+					accum[(n+1) * (numrho + 2) + r + 1]++;
 				}
 			}
 		}
 	}
+
+	//theta方向、rho方向のどちらにおいても極大値となる組み合わせのみをとりだして、類似する線を削除する
+	std::vector<int> lm_indices;
+	findLocalMaximum(numrho, numangle, threshold, accum, lm_indices);
+	
+	
 }
 
