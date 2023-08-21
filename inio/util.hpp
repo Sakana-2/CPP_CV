c51aefcb24b661cacbdb1d01fba02b6c44f2fc8a
Sakana <45968398+Sakana-2@users.noreply.github.com>
2023-08-21 11:56:50 +0900

    Deleted release bulid mode.

--- inio/util.hpp
+++ inio/util.hpp
@@ -1,10 +1,5 @@
 #pragma once
 
-#ifdef NDEBUG
-#include <pybind11/pybind11.h>
-#include <pybind11/stl.h>
-#endif
-
 #include <cmath>
 #include <opencv2/opencv.hpp>
 
@@ -17,9 +12,3 @@ inline double gaussian(double s, double rr) {
 cv::Mat mat_nearbyint(cv::Mat src);
 
 cv::Mat mat_clump(cv::Mat src);
-
-#ifdef NDEBUG
-pybind11::dict mat2dict(cv::Mat src);
-
-cv::Mat dict2mat(pybind11::dict src);
-#endif // NDEBUG
