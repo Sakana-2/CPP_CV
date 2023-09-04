3296c16ac5dbd230ad7711a866f4eeaf59cbb33a
Sakana <45968398+Sakana-2@users.noreply.github.com>
2023-09-04 18:48:22 +0900

    Made inio_desktop be able to open image

--- inio_desktop/inio_desktop.cpp
+++ inio_desktop/inio_desktop.cpp
@@ -7,6 +7,17 @@ inio_desktop::inio_desktop(QWidget *parent)
 	ui.setupUi(this);
 }
 
+void inio_desktop::actionsEnabled() {
+	QList<QMenu*> bars = ui.menuBar->findChildren<QMenu*>(QString(), Qt::FindDirectChildrenOnly);
+	for (QMenu* bar : bars)
+	{
+		for (QAction* action : bar->actions())
+		{
+			action->setEnabled(true);
+		}
+	}
+}
+
 void inio_desktop::on_Open_triggered() {
 	QString filename = QFileDialog::getOpenFileName(
 		this,
@@ -14,7 +25,34 @@ void inio_desktop::on_Open_triggered() {
 		"",
 		tr("Images(*.jpg *.jpeg *.png)")
 	);
+	if (filename.isEmpty()) {
+		return;
+	}
+	img.load(filename.toLocal8Bit().toStdString());
+	reload();
+	actionsEnabled();
+}
 
+void inio_desktop::reload() {
+	cv::Mat src;
+	cv::Mat data = img.get();
+	if (data.type() == 6) {
+		data.convertTo(data,CV_8UC1);
+		cv::cvtColor(data, src, cv::COLOR_GRAY2BGR);
+	}
+	else {
+		data.convertTo(data, CV_8UC3);
+		src = data;
+	}
+	
+	qimg = QImage(src.data, src.cols, src.rows, src.step, QImage::Format_BGR888);
+	displayImage();
+}
+
+void inio_desktop::displayImage() {
+	int w = ui.image->width();
+	int h = ui.image->height();
+	ui.image->setPixmap(QPixmap::fromImage(qimg.scaled(w, h, Qt::KeepAspectRatio)));
 }
 
 inio_desktop::~inio_desktop()
--- inio_desktop/inio_desktop.h
+++ inio_desktop/inio_desktop.h
@@ -1,7 +1,11 @@
 #pragma once
 
 #include <QtWidgets/QMainWindow>
+#include <qpixmap.h>
+#include <qgraphicsscene.h>
+#include <opencv2/opencv.hpp>
 #include "ui_inio_desktop.h"
+#include "../inio/main.hpp"
 
 class inio_desktop : public QMainWindow
 {
@@ -16,4 +20,10 @@ private slots:
 
 private:
 	Ui::inio_desktopClass ui;
+	Inio img;
+	QImage qimg;
+
+	void actionsEnabled();
+	void reload();
+	void displayImage();
 };
