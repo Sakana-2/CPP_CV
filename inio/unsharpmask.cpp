2fe3b61f6ff9331bb574abb2099d1638fd1c7121
Sakana <45968398+Sakana-2@users.noreply.github.com>
2023-10-31 18:42:59 +0900

    Fix a function of unsharp mask by laplacian.

--- inio/unsharpmask.cpp
+++ inio/unsharpmask.cpp
@@ -1,3 +1,4 @@
+#include "edge_detection.hpp"
 #include "filter2d.hpp"
 #include "global.hpp"
 #include "unsharpmask.hpp"
@@ -5,29 +6,32 @@
 
 cv::Mat um_laplacian(cv::Mat src, int laplacian_mode, double k, int c) {
 	cv::Mat kernel = cv::Mat::zeros(3, 3, CV_64FC1), dst = cv::Mat::zeros(src.rows, src.cols, CV_64FC1);
+	cv::Mat sorry = cv::Mat::ones(5, 5, CV_64FC1);
+	sorry.at<double>(2, 2) = 10;
+	sorry.at<double>(1, 3) = 16;
 	switch (laplacian_mode)
 	{
 	case LAPLACIAN_4:
 		kernel.at<double>(0, 0) = 0;
-		kernel.at<double>(1, 0) = -1;
+		kernel.at<double>(1, 0) = -k;
 		kernel.at<double>(2, 0) = 0;
-		kernel.at<double>(0, 1) = -1;
+		kernel.at<double>(0, 1) = -k;
 		kernel.at<double>(1, 1) = 1+4*k;
-		kernel.at<double>(2, 1) = -1;
+		kernel.at<double>(2, 1) = -k;
 		kernel.at<double>(0, 2) = 0;
-		kernel.at<double>(1, 2) = -1;
+		kernel.at<double>(1, 2) = -k;
 		kernel.at<double>(2, 2) = 0;
 		break;
 	case LAPLACIAN_8:
-		kernel.at<double>(0, 0) = -1;
-		kernel.at<double>(1, 0) = -1;
-		kernel.at<double>(2, 0) = -1;
-		kernel.at<double>(0, 1) = -1;
+		kernel.at<double>(0, 0) = -k;
+		kernel.at<double>(1, 0) = -k;
+		kernel.at<double>(2, 0) = -k;
+		kernel.at<double>(0, 1) = -k;
 		kernel.at<double>(1, 1) = 1+8*k;
-		kernel.at<double>(2, 1) = -1;
-		kernel.at<double>(0, 2) = -1;
-		kernel.at<double>(1, 2) = -1;
-		kernel.at<double>(2, 2) = -1;
+		kernel.at<double>(2, 1) = -k;
+		kernel.at<double>(0, 2) = -k;
+		kernel.at<double>(1, 2) = -k;
+		kernel.at<double>(2, 2) = -k;
 		break;
 	default:
 		break;
